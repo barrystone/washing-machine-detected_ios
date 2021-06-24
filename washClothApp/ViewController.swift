29dc4d82cee4549f2feee493e1f9c2cedf9f04ea
barrystone <barrystone3183@gmail.com>
2021-06-24 17:25:53 +0800

    [add] app necessary icon

--- washClothApp/ViewController.swift
+++ washClothApp/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var doingText1: UILabel!
+    
+    @IBOutlet weak var doingText2: UILabel!
+    
+    @IBOutlet weak var doingTime: UILabel!
+    
+    @IBOutlet weak var finishText: UILabel!
+    
+    @IBOutlet weak var doingImage: UIImageView!
+    
+    @IBOutlet weak var finishImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
+       
+
     }
+    
 
-
+    
+   
+    
+    
 }
 
